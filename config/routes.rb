88c96e12c1d1cce0549e4b5efe22b213987d5123
Angelo <angelomilienjoseph@gmail.com>
2021-10-19 20:55:07 -0400

    added resources vehicles to config/routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :rental_companies
+  resources :vehicles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
